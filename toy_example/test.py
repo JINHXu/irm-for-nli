b7c3638bc1f041716de3d555d9deec92727ca3c6
JINHXu <xujinghua1998@gmail.com>
2022-11-10 10:53:23 +0100

    update test test data

--- toy_example/test.py
+++ toy_example/test.py
@@ -2,7 +2,17 @@ from dataset_utils import NLIDataset
 import numpy as np
 from torch.utils.data import DataLoader
 
-d = NLIDataset(1000)
+# d = NLIDataset(100)
+
+# test data setup
+seed = np.random.randint(0, 2 ** 31)
+rng = np.random.RandomState(seed)
+noise=0.0 
+biased_samples_ratio=1.0 
+env_prob=0.0
+bs_test=32
+
+# d = NLIDataset(1000, noise=noise, biased_samples_ratio=biased_samples_ratio, prob=env_prob, rng=rng)
 
 # samples = d._generate_samples(rng = np.random.RandomState())
 # print(samples)
@@ -11,16 +21,26 @@ d = NLIDataset(1000)
 
 # print(samples)
 
-seed = None
-rng = np.random.RandomState(seed)
-noise=0.0
-biased_samples_ratio=1.0
-train_env_prob=(0.8, 0.9)
-bs_train=32
+# seed = None
+# rng = np.random.RandomState(seed)
+# noise=0.0
+# biased_samples_ratio=1.0
+# train_env_prob=(0.8, 0.9)
+# bs_train=32
 
-ds_train = [NLIDataset(10000, noise=noise, biased_samples_ratio=biased_samples_ratio, prob=q, rng=rng)
-            for q in train_env_prob]
+# ds_train = [NLIDataset(10000, noise=noise, biased_samples_ratio=biased_samples_ratio, prob=q, rng=rng)
+#             for q in train_env_prob]
 
-dl_train = [DataLoader(env, batch_size=bs_train, shuffle=True) for env in ds_train]
+# dl_train = [DataLoader(env, batch_size=bs_train, shuffle=True) for env in ds_train]
 
-print(dl_train)
+# print(dl_train)
+
+ds_test = NLIDataset(1000, noise=noise, biased_samples_ratio=biased_samples_ratio, prob=env_prob, rng=rng)
+# dl_test = [DataLoader(ds_test, batch_size=bs_test)]
+
+print(ds_test.samples)
+
+# for x in dl_test:
+#     print(x)
+
+
